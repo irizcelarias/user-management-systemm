e37f26e9622d357feafd95287629572105325948
Irizcel Arias <irizcelarias122917@gmail.com>
2025-05-19 12:16:08 +0800

    deleting conflict files

--- Angular-10-Boilerplate/src/app/_models/employee.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Employee {
-  id: number;
-  employeeId: string;
-  user: { email: string };
-  position: string;
-  department: { name: string };
-  hireDate: string;
-  status: string;
-}
--- Angular-10-Boilerplate/src/app/admin/subnav.component.ts
+++
@@ -1,8 +0,0 @@
-// src/app/admin/accounts/subnav.component.ts
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-subnav',
-  templateUrl: './subnav.component.html',
-})
-export class SubNavComponent {}
